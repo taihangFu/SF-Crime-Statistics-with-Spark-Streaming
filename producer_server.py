54cfb19c35af56a0979353c0d21adef4b981125c
taihangFu <tatsonreload@icloud.com>
2020-02-25 09:43:35 +0000

    added code to decode JSON file

--- producer_server.py
+++ producer_server.py
@@ -13,11 +13,12 @@ class ProducerServer(KafkaProducer):
     #TODO we're generating a dummy data
     def generate_data(self):
         with open(self.input_file) as f:
-            for line in f:
-                message = self.dict_to_binary(line)
+            json_data = json.load(f)
+            for row  in json_data:
+                message = self.dict_to_binary(row)
                 # TODO send the correct data
                 self.send(self.topic, message)
-                time.sleep(0.1)
+                time.sleep(1)
 
     # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
